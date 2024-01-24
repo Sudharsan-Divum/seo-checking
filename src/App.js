34ee3d5b76351756692aff6b821cd537aca6da1b
Sudharsan-Divum <sudharsan.selvasekar@divum.in>
2024-01-24 09:45:55 +0530

    Meta tags added

--- src/App.js
+++ src/App.js
@@ -6,8 +6,25 @@ export default function App() {
   return (
     <>
       <Helmet>
-        <Routing />
+        <meta
+          name="description"
+          content="Explore a website created for comprehensive SEO testing and optimization analysis purposes."
+        />
+        <link
+          rel="canonical"
+          href="https://prismatic-narwhal-ee86e9.netlify.app"
+        />
+        <meta name="og: title" content="Hostifier" />
+        <meta
+          name="og: description"
+          content="Explore a website created for comprehensive SEO testing and optimization analysis purposes, named Hostifier"
+        />
+        <meta
+          name="keywords"
+          content="poc, POC, this, that, entry, nav, inin,rro,sseeoo"
+        />
       </Helmet>
+      <Routing />
     </>
   );
 }
